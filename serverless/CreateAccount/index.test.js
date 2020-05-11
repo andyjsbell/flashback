d6966731323d6b782cb3c3238fa8ead0f11c2879
Andy Bell <andy.bell@barkingmad.io>
2020-05-11 16:58:08 +0200

    Create account with just email

--- serverless/CreateAccount/index.test.js
+++ serverless/CreateAccount/index.test.js
@@ -1,21 +1,15 @@
 const httpFunction = require('./index');
 const context = require('../testing/defaultContext');
-const StellarSdk = require('stellar-sdk');
 
 it('should create account and have balance of 1 lumens', async () => {
     jest.setTimeout(30000);
-    const pair = StellarSdk.Keypair.random();
 
     const request = {
         body: {
-            publicKey: pair.publicKey(),
             email: "a@a.com"
         }
     };
 
-    console.log(`PK:${pair.publicKey()} Secret:${pair.secret()}`)
-
     await httpFunction(context, request);
     expect(context.res.body.status).toEqual("Succeeded");
-    console.log(context.res.body.transactionResult._links.transaction.href);
 });
