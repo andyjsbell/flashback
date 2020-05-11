4f0ca368ecee2d5e9cce11b10560453e35b0b947
Andy Bell <andy.bell@barkingmad.io>
2020-05-11 17:25:04 +0200

    login with no auth, for the moment

--- serverless/LoginAccount/index.js
+++ serverless/LoginAccount/index.js
@@ -1,16 +1,46 @@
+const { validateEmail } = require('../emails');
+
+// Grabs PKs for email, authentication to be added
 module.exports = async function (context, req) {
     context.log('LoginAccount HTTP trigger function processed a request.');
 
-    if (req.query.name || (req.body && req.body.name)) {
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
-        };
-    }
-    else {
+    if (req.body && req.body.email) {
+        const newEmail = req.body.email;
+        if (validateEmail(newEmail)) {
+
+            const accounts = context.bindings.accountsTable.filter(row => {
+                return (row.PartitionKey === req.query.email);
+            });
+
+            if (accounts.length > 0) {
+                let publicKeys = [];
+                accounts.forEach(account => publicKeys.push(account.RowKey));
+                context.res = {
+                    body: {
+                        status: "Succeeded",
+                        publicKeys
+                    }
+                };
+            } else {
+                context.res = {
+                    status: 400,
+                    body: {
+                        status: "Error",
+                        message: "email not found"
+                    }
+                };
+            }
+
+        } else {
+            context.res = {
+                status: 400,
+                body: "Please pass a valid email"
+            };
+        }
+    } else {
         context.res = {
             status: 400,
-            body: "Please pass a name on the query string or in the request body"
+            body: "Please pass a valid body"
         };
     }
 };
